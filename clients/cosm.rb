3213101bc9a4f2847b450a8249fcdde2894b2737
David Michael <david.michael@gmail.com>
2013-02-08 17:57:03 -0500

    hm

--- clients/cosm.rb
+++ clients/cosm.rb
@@ -2,8 +2,6 @@ require 'eventmachine'
 require File.join File.dirname(__FILE__), 'pachube-stream/lib/pachube-stream'
 
 EventMachine.run do
-
-  ENV["PACHUBE_API_KEY"] = "qm6x09udPhk9QWucaBgLXJ3AtU6SAKxsSlFOTGJuSTl3cz0g"
   connection = PachubeStream::Connection.connect(:api_key => ENV["PACHUBE_API_KEY"])
 
   request = connection.subscribe("/feeds/103394")
